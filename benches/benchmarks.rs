349c5b0bb4f3ece19047fe39226ad857621dcf49
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2024-04-25 17:18:11 +0200

    what was i thinking...

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -3,12 +3,12 @@ use std::path::PathBuf;
 use toy_payments_engine::run_transactions_from_file;
 
 fn criterion_benchmark(c: &mut Criterion) {
-    // c.bench_function("transactions 100mb", |b| {
-    //     let mut input_path = PathBuf::from(file!());
-    //     input_path.pop();
-    //     input_path.push("transactions_100mb.csv");
-    //     b.iter(|| run_transactions_from_file(&input_path))
-    // });
+    c.bench_function("transactions 100mb", |b| {
+        let mut input_path = PathBuf::from(file!());
+        input_path.pop();
+        input_path.push("transactions_100mb.csv");
+        b.iter(|| run_transactions_from_file(&input_path))
+    });
 
     c.bench_function("transactions 250", |b| {
         let mut input_path = PathBuf::from(file!());
